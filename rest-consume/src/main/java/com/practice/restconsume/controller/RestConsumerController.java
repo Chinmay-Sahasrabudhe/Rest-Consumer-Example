328c495ef0256d10fde53cf742643a34810c0198
Chinmay-Sahasrabudhe <c.s.sahasrabudhe@gmail.com>
2022-04-01 18:16:37 +0530

    Update

--- rest-consume/src/main/java/com/practice/restconsume/controller/RestConsumerController.java
+++ rest-consume/src/main/java/com/practice/restconsume/controller/RestConsumerController.java
@@ -13,25 +13,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.practice.restconsume.bean.ApiResponseBean;
 import com.practice.restconsume.service.RestConsumerService;
 
+
 @RestController
 public class RestConsumerController {
 	
 	@Autowired
 	private RestConsumerService service;
 	
+	/**
+	 * 
+	 * @return
+	 */
 	@GetMapping(path = "/cf", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> getApiData() {
-		
-		List<ApiResponseBean> listResponse = null;
-		
-		listResponse = service.getDataFromApi();
-			return new ResponseEntity<Object>(listResponse,HttpStatus.OK);  
+	public ResponseEntity<List<ApiResponseBean>> getApiData() {
+		System.out.println("Inside getApiData");
+		List<ApiResponseBean> listResponse = service.getDataFromApi();
+		System.out.println("Inside getApiData END");
+		return new ResponseEntity<List<ApiResponseBean>>(listResponse,HttpStatus.OK);  
 	}
 	
+	/**
+	 * 
+	 * @param provider
+	 * @return
+	 */
 	@GetMapping(path = "/cf/{provider}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> getDataByProvider(@PathVariable String provider) {
-		ApiResponseBean apiRun = null;
-		apiRun = service.getDataByProvider(provider);
-			return new ResponseEntity<Object>(apiRun, HttpStatus.OK);
+	public ResponseEntity<ApiResponseBean> getDataByProvider(@PathVariable String provider) {
+		System.out.println("Inside getDataByProvider");
+		ApiResponseBean apiRun  = service.getDataByProvider(provider);
+		System.out.println("Inside getDataByProvider END");
+		return new ResponseEntity<ApiResponseBean>(apiRun, HttpStatus.OK);
 	}
 }
--- rest-consume/src/main/java/com/practice/restconsume/service/RestConsumerService.java
+++ rest-consume/src/main/java/com/practice/restconsume/service/RestConsumerService.java
@@ -17,7 +17,6 @@ import com.practice.restconsume.Exception.InternalServerError;
 import com.practice.restconsume.Exception.InvalidProviderException;
 import com.practice.restconsume.Exception.ServiceTimedOutException;
 import com.practice.restconsume.bean.ApiResponseBean;
-import com.practice.restconsume.bean.ExceptionBean;
 
 @Service
 public class RestConsumerService {
@@ -31,9 +30,12 @@ public class RestConsumerService {
 	@Value("${bluemix.url}")
 	private String blueUrl;
 	
-	@SuppressWarnings("finally")
+	/**
+	 * 
+	 * @return List
+	 */
 	public List<ApiResponseBean> getDataFromApi() {
-		
+		System.out.println("Inside getDataFromApi service");
 		List<ApiResponseBean> listApiResponse = new ArrayList<ApiResponseBean>();
 		ObjectMapper mapper = new ObjectMapper();
 		ResponseEntity<String> response = null;
@@ -41,29 +43,36 @@ public class RestConsumerService {
 		
 		try {
 				//calling pwc url
+				System.out.println("getDataFromApi service - calling PWC api");
 				response = restTemplate.getForEntity(pwcUrl, String.class);
 				ApiResponseBean bean = null;
+				System.out.println("getDataFromApi service - Status Code " +response.getStatusCode());
 				if(response.getStatusCode() == HttpStatus.OK) {
 					responseJsonStr = response.getBody().toString();
 					bean = mapper.readValue(responseJsonStr, ApiResponseBean.class);
 					listApiResponse.add(bean);
 				} else {
+					System.out.println("getDataFromApi service - Invalid response status code for PWC");
 					throw new InternalServerError("Internal Server Error");
 				}
 				
 				//calling Bluemix url
+				System.out.println("getDataFromApi service - calling Bluemix api");
 				response = restTemplate.getForEntity(blueUrl, String.class);
 				ApiResponseBean bean1 = null;
+				System.out.println("getDataFromApi service -Bluemix Status Code " +response.getStatusCode());
 				if(response.getStatusCode() == HttpStatus.OK) {
 					responseJsonStr = response.getBody().toString();
 					bean1 = mapper.readValue(responseJsonStr, ApiResponseBean.class);
 					listApiResponse.add(bean1);
 				} else {
+					System.out.println("getDataFromApi service - Invalid response status code for Bluemix");
 					throw new InternalServerError("Internal Server Error");
 				}
-			return listApiResponse;
+				System.out.println("getDataFromApi service END");
+				return listApiResponse;
 		} catch(Exception ex) {
-			
+			ex.printStackTrace(); //for local test
 			if(ex instanceof ResourceAccessException && ex.getCause() instanceof SocketTimeoutException) {
 				throw new ServiceTimedOutException("service timed out.");
 			} else {
@@ -72,33 +81,43 @@ public class RestConsumerService {
 		}
 	}
 	
-	
+	/**
+	 * 
+	 * @param provider
+	 * @return ApiResponseBean
+	 */
 	public ApiResponseBean getDataByProvider(String provider) {
-		
+		System.out.println("Inside getDataByProvider for provider - " +provider);
 		String url = null;
 		if("pwc".equalsIgnoreCase(provider) ) {
 			url = pwcUrl;
 		} else if("blu".equalsIgnoreCase(provider)) {
 			url = blueUrl;
 		} else {
+			System.out.println("getDataByProvider - Invalid provider input");
 			throw new InvalidProviderException("Invalid Provider Entered");
 		}
-		
+		System.out.println("getDataByProvider API - " + url);
 		ObjectMapper mapper = new ObjectMapper();
 		ResponseEntity<String> response = null;
 		String responseJsonStr = null;
 		ApiResponseBean bean = null;
 		
 		try {
+				System.out.println("Calling api - "+ url);
 				response = restTemplate.getForEntity(url, String.class);
+				System.out.println("getDataByProvider - api response status code "+ response.getStatusCode());
 				if(response.getStatusCode() == HttpStatus.OK) {
 					responseJsonStr = response.getBody().toString();
 					bean = mapper.readValue(responseJsonStr, ApiResponseBean.class);
 				} else {
+					System.out.println("getDataByProvider - api response status code is not 200 OK");
 					throw new InternalServerError("Internal Server Error");
 				}
+				System.out.println("getDataByProvider END");
 				return bean;
 			} catch(Exception ex) {
+				ex.printStackTrace(); //for local
 				if(ex instanceof ResourceAccessException && ex.getCause() instanceof SocketTimeoutException) {
 					throw new ServiceTimedOutException("service timed out.");
 				} else {
